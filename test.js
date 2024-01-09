76c4c41f9d98ff70b56b52e897af333d1b5af327
skjaguar2030 <seifira21@protonmail.com>
2024-01-09 18:21:20 +0200

    testing another branch

--- test.js
+++ test.js
@@ -2,5 +2,4 @@ const x= 'just testing'
 const y= 'adding more content'
 const z= 'adding more and more content'
 const a= 'testing another branch'
-const b=  
-const b= 'the alternative branch is called "\test"\ 
+const b= 'the alternative branch is called "test"'
